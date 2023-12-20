ad687b4fad03fa01468d321ca36349eef54ff12c
Aderajew Yeshiwendm <aderajewy26@gmail.com>
2023-12-20 06:40:18 -0900

    first commit

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
 let express = require('express');
 let app = express();
+app.get('/', (req,res)=>{
+    console.log('Hello world');
+   
+})
+// app.listen(3000,()=>console.log('Hello World'))
+
 
 
 
