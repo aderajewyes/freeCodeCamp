566708211d106e912b726add2e182134e9375e5f
Aderajew Yeshiwendm <aderajewy26@gmail.com>
2023-12-20 07:02:21 -0900

    second commit

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 app.get('/', (req,res)=>{
-    console.log('Hello world');
+    console.log('Hello World');
    
 })
 // app.listen(3000,()=>console.log('Hello World'))
